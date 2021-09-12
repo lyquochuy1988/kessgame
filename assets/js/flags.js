e2f5613d5d7afddb7a6b1955a03728127dd2f880
lyquochuy1988 <lyquochuy2701>
2021-09-12 11:20:15 +0700

    fix responsive

---
+++ assets/js/flags.js
@@ -0,0 +1,20 @@
+// const $ = document.querySelector.bind(document)
+// const $$ = document.querySelectorAll.bind(document)
+
+// const links = $$('.flags ul li a')
+//     // const panes = $$('.pane')
+
+// links.forEach(function(item, index) {
+
+//     // const pane = panes[index]
+
+//     item.onclick = function(event) {
+//         return false;
+
+//         // $('.tab-item.active').classList.remove('active')
+//         // $('.pane.active').classList.remove('active')
+
+//         // this.classList.add('active')
+//         // pane.classList.add('active')
+//     }
+// })
